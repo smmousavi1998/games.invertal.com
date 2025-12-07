edcdbe8953fe25809cbb10affe88e730232bc361
smmousavi1998 <92441906+smmousavi1998@users.noreply.github.com>
2025-12-07 22:19:26 +0400

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import '@radix-ui/themes/styles.css';
+import { Theme } from '@radix-ui/themes';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './src/pages/Home';
+import Reservations from './src/pages/Reservations';
+import About from './src/pages/About';
+import Gallery from './src/pages/Gallery';
+import Contact from './src/pages/Contact';
+import NotFound from './src/pages/NotFound';
+
+const App: React.FC = () => {
+  return (
+    <Theme appearance="inherit" radius="large" scaling="100%">
+      <Router>
+        <main className="min-h-screen font-inter">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/reservations" element={<Reservations />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          <ToastContainer
+            position="top-right"
+            autoClose={3000}
+            newestOnTop
+            closeOnClick
+            pauseOnHover
+          />
+        </main>
+      </Router>
+    </Theme>
+  );
+}
+
+export default App;
